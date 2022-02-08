c48348ce65caa33fc7294d93e9cc567df80215db
BinaryVixen899 <lealac@msn.com>
2022-02-07 19:58:08 -0800

    100th times the charm

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,39 +1,27 @@
-terraform {
-  experiments = [module_variable_optional_attrs]
-}
+# terraform {
+#   experiments = [module_variable_optional_attrs]
+# }
 
 variable "backends" {
     type = list(object({
         name = string
         address = string
-        shield = optional(string)
-        ssl_check_cert = optional(bool)
-        weight = optional(number)
+        weight = number
+        auto_loadbalance = bool
+        shield = string
+        ssl_check_cert = bool
     }))
-    default = [ {
-        name = "required"
-        address = "required"
-        shield = "pao-ca-us"
-        ssl_check_cert = true
-        # UHHHH, check this out 
-        weight = 100
-    }]
+    
+#    and thus ends our escapade with default values. I want to point out here for future reference that the reason this didn't work was because of object types and how default works. The way to get it to work would be to use the Defaults() option, but that would require more knowledge about types. Come back and fix this later. There is a way and you can figure it out. 
 }
+
 variable "conditions" {
-    type=list(object({
+    type = list(object({
         name = string
         statement = string
         type = string
-        priority = optional(string)
-        
-        
+        priority = string
     }))
-    default = [ {
-      name = "value"
-      statement = "value"
-      type = "value"
-      priority = "10"
-    } ]
 }
 
 
@@ -54,30 +42,21 @@ variable "headers" {
         destination = string
         name = string
         type = string
-        ignore_if_set = optional(bool)
-        priority = optional(number)
-        request_condition = optional(string)
-        response_condition = optional(string)
-        source = optional(string)
+        ignore_if_set = bool
+        priority = number
+        request_condition = string
+        response_condition = string
+        source = string
     }))
-    default = [ {
-      action = "required"
-      destination = "required"
-      name = "required"
-      type = "required"
-      ignore_if_set = true
-      priority = 10
-      request_condition = "Purge"
-    } ]
 }
 
-variable "healthchecks" {
-    type=list(object({
-        host = string
-        name = string
-        path = string
-    }))
-}
+# variable "healthchecks" {
+#     type=list(object({
+#         host = string
+#         name = string
+#         path = string
+#     }))
+# }
 
 variable "honeycomb_pass" {
     description = "The honeycomb password"
@@ -113,15 +92,20 @@ variable "snippets" {
         content = string
         name = string
         type = string
-        priority = optional(string)
+        priority = string
     }))
-    default = [{
-        content = "required"
-        name = "required"
-        type = "required"
-        priority = "100"
-    }]
+
 }
 
+# locals { 
+# backends = defaults(var.backends, [ {
+#       address = "value"
+#       name = "value"
+#       weight = 60
+#     } ]
+# )
+# }
 
-#   may have to add a default value
+# it believes it's a tuple 
+# https://www.terraform.io/language/functions/defaults
+# https://stackoverflow.com/questions/65497709/terraform-experiment-release-module-variable-optional-attrs-variable-with-nes
