61c97d7bb8af8d3a7d9d96a87dbe424bb5e93b5d
BinaryVixen899 <lealac@msn.com>
2022-04-23 19:17:59 -0700

    starting to do secondary character pages

---
+++ Profiles/SecondaryCast/alexis.js
@@ -0,0 +1,19 @@
+window.onload() = function() {
+    random
+}
+
+const {
+    randomInt
+} = await import('crypto');
+  
+  randomInt(3, (err, n) => {
+    if (err) throw err;
+    console.log(`Random number chosen from (0, 1, 2): ${n}`);
+    if (n == 2)  {
+       var elem = document.getElementById(laurieplayer)
+       elem.removeAttribute(hidden)
+        
+    }
+  });
+
+  
